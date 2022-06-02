8cc4155b84d3ec090a3c3fecc126c3c7a35ae056
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2022-06-02 12:28:51 -0500

    upload js files

---
+++ js/form.js
@@ -0,0 +1,5 @@
+const adminForm = document.getElementById("admform");
+console.log(adminForm);
+const button = document.getElementById("send")
+
+button.addEventListener("click", e => {console.log("a")})
---
+++ js/main.js
@@ -0,0 +1,3 @@
+const { adminId, adminPassword, userId, userPassword } = require('./loginadm.json');
+console.log(adminId, adminPassword, userId, userPassword);
+setInterval(()=> console.log("a"), 5000)
